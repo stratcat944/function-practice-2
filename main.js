1c579decec24cf7a6448817da18ba7f0735b2da4
Steven Domenico <stratcat944@gmail.com>
2015-07-02 16:58:41 -0600

    next exercise

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 // 1. getName
 
-var getName = function(person) {
 
-	return person.name;
-	// or
-	// return person['name']
+// var getName = function(person) {
 
-}
+// 	return person.name;
+// 	// or
+// 	// return person['name']
 
-console.log(getName({ name: 'Luisa', age: 25 }))
+// }
+
+// console.log(getName({ name: 'Luisa', age: 25 }))
+
 
 // 2. totalLetters
 
-var totalLetters = 
+var stringWords = ['This', 'shit', 'is', 'really', 'starting', 'to', 'piss', 'me', 'off']
+
+var totalLetters = 0
+
+for(i = 0; i < stringWords.length; i++) {
+	totalLetters+=stringWords[i].length;
+}
+
+console.log(totalLetters);
